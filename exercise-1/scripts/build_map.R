f38e728e86d3d4149a7d4c2e3834c62bd312c48e
Gina Lee <Gina Lee>
2016-02-19 14:52:02 -0800

    mods to exercise-1

---
+++ exercise-1/scripts/build_map.R
@@ -0,0 +1,30 @@
+library(plotly)
+library(jsonlite)
+
+
+build_map <- function(data) {
+  g <- list(
+    scope = 'usa', 
+    projection = list(type = 'albers usa'),
+    showland = FALSE,
+    landcolor = toRGB("gray85"), 
+    subunitwidth = 1,
+    countrywidth = 0,
+    subunitcolor = toRGB("gray85"), 
+    countrycolor = toRGB('gray85')
+  )
+  
+  
+  # Create graph
+  plot_ly(data, 
+          lon = lng, 
+          lat = lat, 
+          text = paste('<b>Name:</b>', data[,"Victim Name"], '<br><b>Shots Fired:</b>', data$shots_fired),
+          hoverinfo = 'text',
+          marker = list(size = 1 + shots_fired/3, opacity = .4, color = 'red'),
+          type = 'scattergeo', 
+          locationmode = 'USA-states'
+  ) %>%
+    layout(title = 'Crowdsourced Police Shootings', geo = g) %>% 
+    return
+  }
